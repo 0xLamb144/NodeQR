c11f52e9786c6add21262ea80968c239bf3db431
alvinjohnson <alvinjohnson144@gmail.com>
2024-06-24 00:25:10 -0400

    modules added to index.js file

---
+++ index.js
@@ -0,0 +1,6 @@
+// Import inquirer from - https://www.npmjs.com/package/inquirer
+import inquirer from "inquirer";
+// Import the qr-image package from-https://www.npmjs.com/package/qr-image
+import qr from "qr-image"
+// Importing the File system module from Node.js - https://nodejs.org/docs/latest/api/fs.html
+import fs from "fs";
